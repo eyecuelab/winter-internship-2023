282469facbbac61b939ab858b553e365d072746e
Helen Lehrer <hlehrer@outlook.com>
2023-01-26 14:11:54 -0800

    Add Vite & React initial client structure

---
+++ client/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+  return (
+  <>
+  <div className="wrapper">
+  <h1>Welcome To My App</h1>
+  <p>This is going to be the coolest app in the world!</p>
+  </div>
+  </>
+  );
+  }
