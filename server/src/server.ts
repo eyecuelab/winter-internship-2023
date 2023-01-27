cdf67b7b1e6e94ebfcfdae6fd94eac8b64163e27
josephjacksonw <josephjacksonw@gmail.com>
2023-01-26 20:04:23 -0500

    suffered
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,35 @@
 import app from './app';
+//import express from 'express';
+//const app = express();
 import http from 'http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+//import cors from 'cors';
+
+//app.use(cors());
+//lines 3,6, and 8 take everything that we would have made in app.ts
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
   }
 });
 
+
+
 io.on("connection", (socket) => {
   console.log("User Connected: " + socket.id)
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+    console.log(data);
+  })
+})
+
+// io.on("connection", (socket) => {
+//   console.log("User Connected: " + socket.id)
 
 
   /*
@@ -24,10 +41,10 @@ io.on("connection", (socket) => {
   socket.broadcast.emit("receive_message", data); = sends the data from the argument in send_message out as the name receive_message, so that the client side can run receive_message
   })
   */
-})
+//})
 
 export default io;
 
-server.listen(3000, () =>
-  console.log('Server ready at: http://localhost:3000'),
+server.listen(3001, () =>
+  console.log('Server ready at: http://localhost:3000 or maybe 3001 idk'),
 )
