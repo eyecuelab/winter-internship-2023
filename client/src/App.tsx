09fe7735a3597e20cf18f4c02f300d08b0bd4471
Patty Rose <oteropatty@gmail.com>
2023-01-27 16:01:41 -0800

    add event listeners BUG: key events unexpected results

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/login/Login";
 import Canvas from "./components/canvas/Canvas";
 import Lobby from "./components/lobby/Lobby";
+import { useState } from 'react';
 
 function App() {
+
   return (
     <Router>
       <Routes>
