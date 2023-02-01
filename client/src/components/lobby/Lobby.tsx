3b9c851476602862c5b2a4a8fd7da1221f8cfc58
Helen Lehrer <hlehrer@outlook.com>
2023-02-01 09:28:20 -0800

    Delete comments

--- client/src/components/lobby/Lobby.tsx
+++ client/src/components/lobby/Lobby.tsx
@@ -18,66 +18,16 @@ interface UserDataGoogle {
 	logout: () => void;
 }
 
-const socket = io.connect("http://localhost:3001");
-
 const Lobby = (props: Props) => {
 
   const { updateUserData } = props;
-  //sockets
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
 
-  //user auth
   const [userDataGoogle, setUserDataGoogle] = useState<null | UserDataGoogle>(null)
 
   const loginWith = useRef(localStorage.getItem("loginWith"))
 
   const navigate = useNavigate()
-
-  // every communication that we want to run will be declared here as a const
-  // this one is sendMessage, the "send_message" in the emit is what the backend is looking for
-  // if we want to send data, the message is where we would send it
-  // so with this, "Hello" is the data sent
-  // we will build functions that just need to send the data over to the server based on the specific input
-  // as long as the input receiver is good and differentiated, then none of that really matters what were sending
-  // *unless its data like username or something
   
-  const sendMessage = () => {
-    socket.emit("send_message", {message: message}); //since the message and the variable are the same, you can just do {message}, I left it in there tho in case we want an example on how to send other information
-  };
-
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-      
-      //this below would do a window alert with literally just "Hello" from the sendMessage function, we've changed it since tho
-      //alert(data.message);
-    });
-  }, [socket]);
-
-  //user auth
-  // useEffect(() => {
-  //   const queryString = window.location.search
-  //   const urlParams = new URLSearchParams(queryString)
-  //   const codeParam = urlParams.get("code")
-  
-  //   const accessToken = localStorage.getItem("accessToken")
-  
-  //   if (codeParam && !accessToken && loginWith.current === "GitHub") {
-  //    getAccessTokenGithub(codeParam).then(resp => {
-  //     localStorage.setItem("accessToken", resp.access_token)
-  //     getUserDataGithub(resp.access_token).then((resp: UserDataGithub) => {
-  //      setUserDataGithub(resp)
-  //     })
-  //    })
-  //   } else if (codeParam && accessToken && loginWith.current === "GitHub") {
-  //    getUserDataGithub(accessToken).then((resp: UserDataGithub) => {
-  //     localStorage.setItem("accessToken", accessToken)
-  //     setUserDataGithub(resp)
-  //    })
-  //   }
-  //  }, [loginWith])
   
    useEffect(() => {
     let tempObj = {
@@ -98,14 +48,6 @@ const Lobby = (props: Props) => {
 
   const accessOrCreateUser = (object: any) => {
 
-    // try{
-    //   getData(`/user/${object.email}`)
-    //   console.log( getData(`/user/${object.email}`));
-    // }
-    // catch {
-    //   postData('/user', { email: object.email, name: object.name })
-    // }
-
     getData(`/user/${object.email}`).then((user) => {
         if (!user) {
           postData('/user', { email: object.email, name: object.name })
@@ -118,7 +60,6 @@ const Lobby = (props: Props) => {
     localStorage.removeItem("loginWith")
     navigate("/")
    }
-  //  if (!userDataGithub && !userDataGoogle) return null
    if (!userDataGoogle) return null
 
    const createUser = (event: any) => {
@@ -128,18 +69,7 @@ const Lobby = (props: Props) => {
 
   return (
     <>
-    {/* sockets */}
-        <div className="App">
-          <input placeholder='Message...' onChange={(event) => {
-            setMessage(event.target.value);
-          }}/>
-          <button onClick={sendMessage}>Send Message</button>
-          <h1>Message: </h1>
-          {messageReceived}
-        </div>
-        <hr/>
-        {/* Google Auth */}
-        <div>
+  <div>    
   <form>
     <label htmlFor="name">Game Display Name:</label>
     <input type="text" placeholder='Name'></input>
