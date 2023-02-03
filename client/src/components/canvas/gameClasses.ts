207c00f607849487afb5c91ce6743b9aadc85b23
Patty Rose <oteropatty@gmail.com>
2023-02-03 12:39:53 -0800

    add Team class to game logic

--- client/src/components/canvas/gameClasses.ts
+++ client/src/components/canvas/gameClasses.ts
@@ -1,8 +1,8 @@
 export class Boundary {
   static width = 40;
   static height = 40;
-  position: { x: number; y: number; };
-  constructor({ position }: {position: {x: number, y: number}}) {
+  position: { x: number; y: number };
+  constructor({ position }: { position: { x: number; y: number } }) {
     this.position = position;
     Boundary.width = 40;
     Boundary.height = 40;
@@ -17,10 +17,16 @@ export class Boundary {
 }
 
 export class Player {
-  position:  {x:number, y:number};
-  velocity:  {x:number, y:number};
+  position: { x: number; y: number };
+  velocity: { x: number; y: number };
   radius: number;
-  constructor({ position, velocity }: {position: {x:number, y:number}, velocity:  {x:number, y:number}}) {
+  constructor({
+    position,
+    velocity,
+  }: {
+    position: { x: number; y: number };
+    velocity: { x: number; y: number };
+  }) {
     this.position = position;
     this.velocity = velocity;
     this.radius = 15;
@@ -39,4 +45,20 @@ export class Player {
   //   this.position.x += this.velocity.x; //updates left/right location by 1 increment of velocity unit
   //   this.position.y += this.velocity.y; ////updates up/down location by 1 increment of velocity unit
   // }
+}
+export class Team {
+  players: { x: string; y: string };
+  playerInControl: string;
+  constructor({ players }: { players: { x: string; y: string } }) {
+    this.players = players;
+    this.playerInControl = "x";
+  }
+
+  changePlayerInControl() {
+    if (this.playerInControl === "x") {
+      this.playerInControl = "y";
+    } else {
+      this.playerInControl = "x";
+    }
+  }
 }
