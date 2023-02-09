510e977adca71200921b29735efc963282e25833
Patty Rose <oteropatty@gmail.com>
2023-02-09 11:10:57 -0800

    add navigate to gameOver modal

--- client/src/components/canvas/gameOver.tsx
+++ client/src/components/canvas/gameOver.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import React, { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function useGameOver() {
   const [isOpen, setisOpen] = useState(false);
@@ -28,10 +29,22 @@ interface GameOverType {
 
 export function GameOver(props: GameOverType) {
   
+  const navigate = useNavigate();
+  const goToLobby = () => {
+    navigate("/lobby");
+  }
+  
   return (
     <>
       {props.isOpen && (
         <div className="gameover-overlay" onClick={props.toggle}>
+          <div>
+            <h1>Game Over</h1>
+            <h2>Team 1: $score</h2>
+            <hr />
+            <h2>Team 2: $score</h2>
+            <button onClick={goToLobby}>Start a Public Game!</button>
+          </div>
           <div onClick={(e) => e.stopPropagation()} className="gameover-box">
             {props.children}
           </div>
