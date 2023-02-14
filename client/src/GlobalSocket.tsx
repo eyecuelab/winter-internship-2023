6e39cf0746295ab2abbaa561e9af3809ceaf02ed
josephjacksonw <josephjacksonw@gmail.com>
2023-02-13 19:08:57 -0500

    fly toml file
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>
    Co-authored-by: Helen Lehrer <hlehrer@outlook.com>
    Co-authored-by: Patty Rose <oteropatty@gmail.com>

--- client/src/GlobalSocket.tsx
+++ client/src/GlobalSocket.tsx
@@ -1,6 +1,6 @@
 import * as io from "socket.io-client";
 
-export const socket = io.connect("https://superpacart.fly.dev");
+export const socket = io.connect("https://super-pacart.fly.dev");
 export let socketId = "";
 socket.on("connect", () => {
   socketId = socket.id;
