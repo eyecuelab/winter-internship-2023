b2143706451fa8de21d6700bbf87edb21cf418dd
Helen Lehrer <hlehrer@outlook.com>
2023-02-28 21:39:23 -0800

    WIP gameover styling

--- client/src/components/canvas/gameOver.tsx
+++ client/src/components/canvas/gameOver.tsx
@@ -26,7 +26,9 @@ export function GameOver(props: GameOverType) {
       return [score[0], score[1] ?? 0];
     });
     
-    const scoresList = document.getElementById("scoresList");
+    const finalScoreHeading = document.createElement('h2');
+
+    const scoresList = document.getElementById("scores-list");
     if (scoresList) {
       while (scoresList.firstChild) {
         scoresList.removeChild(scoresList.firstChild);
@@ -34,9 +36,26 @@ export function GameOver(props: GameOverType) {
       scoresArr.forEach((item) => {
         var li = document.createElement("li");
         li.appendChild(
-          document.createTextNode(`${item[0]} team -- ${item[1]}`)
+          document.createTextNode(`${item[0]} team `)
+        );
+        var teamScore = document.createElement("span");
+        teamScore.innerText = ` ${item[1]}`;
+        teamScore.setAttribute('id','team-score');
+        li.appendChild(
+          teamScore
         );
         scoresList.appendChild(li);
+        if (item[0] === "red") {
+            li.style.color = "#D52527";
+          } else if (item[0] === "orange") {
+            li.style.color = "#D52527";
+          } else if (item[0] === "blue") {
+            li.style.color = "#D52527";
+          } else if (item[0] === "pink") {
+            li.style.color = "#D52527";
+          } else {
+  
+          }
       });
     }
   };
@@ -48,8 +67,9 @@ export function GameOver(props: GameOverType) {
         <div className="gameover-overlay">
           <div onClick={(e) => e.stopPropagation()} className="gameover-box">
             <div>
-              <h1>Game Over</h1>
-              <ul id="scoresList"></ul>
+              <h1 id="game-over-heading">game over</h1>
+              <p id="final-score-heading">final scores</p>
+              <ul id="scores-list"></ul>
               <button onClick={goToLobby}>Back to Lobby</button>
             </div>
           </div>
